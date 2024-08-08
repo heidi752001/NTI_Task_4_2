33bb281d5bdc99a20a9f7898630de48b2b183551
haidy <haidyyasser@gmail.com>
2024-08-08 19:28:50 +0300

    NTI_Task_4_2

---
+++ script.js
@@ -0,0 +1,12 @@
+function dis(x)
+{
+document.getElementById('res').value+=x
+}
+function res()
+{
+    document.getElementById('res').value=eval(document.getElementById('res').value)
+}
+function cls()
+{
+    document.getElementById('res').value='' 
+}
